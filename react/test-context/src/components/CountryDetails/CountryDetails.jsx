2b4e7d21c288400b716e1a206753a265d0773f04
diaslinoh <diaslinoh@gmail.com>
2021-08-15 16:21:37 -0300

    Using react-query

--- react/test-context/src/components/CountryDetails/CountryDetails.jsx
+++ react/test-context/src/components/CountryDetails/CountryDetails.jsx
@@ -1,13 +1,59 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import styles from './CountryDetails.module.scss';
-import CountryContext from '../../context/CountryContext';
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import styles from "./CountryDetails.module.scss";
+import CountryContext from "../../context/CountryContext";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-const CountryDetails = () => {
-  const {country} = useContext(CountryContext);
+async function fetchUsers(tmp) {
+  try {
+    const response = await fetch(
+      `https://restcountries.eu/rest/v2/alpha/${tmp.queryKey[0]}`
+    );
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("ERROR:");
+    console.log(error);
+    throw new error();
+  }
+}
+
+function TheComponent() {
+  const { country } = useContext(CountryContext);
+  const { isLoading, error, data } = useQuery([country], fetchUsers);
+  if (error)
+    return (
+      <div>
+        <div>Ooops!! Some error.</div>
+        <div>Look the console</div>
+      </div>
+    );
+  if (isLoading) return <div>Loading...</div>;
+
   return (
-    <div >
-      Country: {country ? country : "No country selected."}
+    <div>
+      <div>Country: {country ? country : "No country selected."}</div>
+      <br />
+      <div style={{ "font-weight": "900", "font-size": "1.3em" }}>
+        URL:{" "}
+        <a
+          target="_blank"
+          href={`https://restcountries.eu/rest/v2/alpha/${country}`}
+        >
+          https://restcountries.eu/rest/v2/alpha/{country}
+        </a>
+      </div>
+      <pre
+        style={{
+          border: "solid red 1px",
+          padding: "15px",
+          "max-height": "300px",
+          overflow: "auto",
+        }}
+      >
+        {JSON.stringify(data, null, 2)}
+      </pre>
     </div>
   );
 }
@@ -16,4 +62,29 @@ CountryDetails.propTypes = {};
 
 CountryDetails.defaultProps = {};
 
-export default CountryDetails;
+const queryClient = new QueryClient();
+
+export default function CountryDetails() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TheComponent />
+    </QueryClientProvider>
+  );
+}
+
+// import React, { Component, useContext } from 'react';
+// import CountryContext from '../../context/CountryContext';
+// class CountryDetails extends Component {
+
+//   constructor(){
+//      super();
+
+//   }
+//   render() {
+//     return (
+//       <div>Country Details</div>
+//     );
+//   }
+// }
+
+// export default CountryDetails;
--- react/test-context/src/components/HomePage/HomePage.jsx
+++ react/test-context/src/components/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { memo } from 'react';
 // import PropTypes from 'prop-types';
 import styles from './HomePage.module.scss';
 import { CountryProvider } from '../../context/CountryContext';
 import CountryPicker from '../CountryPicker/CountryPicker';
 import CountryDetails from '../CountryDetails/CountryDetails';
 
-const HomePage = () => {
+
+const HomePage = memo( () => {
   console.log("homePAGE");
   return (
     <div className={styles.HomePage}>
@@ -15,9 +16,10 @@ const HomePage = () => {
         <CountryDetails></CountryDetails>
       </CountryProvider>
       <div className={`${styles.random} `}>RANDOM CLASS OF SCSS MODULE</div>
+      
     </div>
   );
-};
+});
 
 HomePage.propTypes = {};
 
